c0137d27312e9adce26ddd7580000fdd25be9475
Your Name <artq1406@gmail.com>
2019-12-04 20:36:14 +0100

    Django Admin Zajecia 3

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from django.utils.translation import gettext_lazy as _
 from contacts.models import Person, Email, Address, Phone
@@ -27,7 +28,38 @@ class EmailInline(admin.TabularInline):
 
 @admin.register(Person)
 class PersonAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'date_of_birth', 'field_born_in_90', 'field_pesel']
+    list_display_links = ['first_name', 'last_name']
+    ordering = ['first_name', 'last_name']
+    search_fields = ['^last_name']
+    autocomplete_fields = ['friends']
     inlines = [AddressInline, PhoneInline, EmailInline]
     radio_fields = {
         'gender': admin.VERTICAL
     }
+    fieldsets = [
+        (None, {'fields': ['first_name', 'last_name']}),
+        (_('Personal data'), {'fields': ['date_of_birth', 'pesel', 'image', 'gender', 'height']}),
+        (_('Other'), {'fields': ['homepage', 'notes', 'friends']}),
+    ]
+
+    def field_born_in_90(self, model):
+        if model.date_of_birth:
+            return model.date_of_birth.strftime('%Y')[:3] == "199"
+
+    field_born_in_90.boolean = True
+
+    def field_pesel(self, model):
+        if model.pesel:
+            return format_html(f'<span style="color:red;">{model.pesel}</span>')
+        return None
+
+    field_pesel.empty_value_display = ""
+
+    class Media:
+        js = [
+            'contacts/person.js'
+        ]
+        css = {'all': [
+            'contacts/person.css'
+        ]}
