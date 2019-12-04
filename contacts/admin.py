203e6bd7f0b29d305c04f4ea5cd8398985e9741e
Your Name <artq1406@gmail.com>
2019-12-04 19:44:30 +0100

    Tabular Inline and Stacked Inline

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,8 +1,33 @@
 from django.contrib import admin
 
-from contacts.models import Person
+from django.utils.translation import gettext_lazy as _
+from contacts.models import Person, Email, Address, Phone
+
+admin.site.site_header = _('Addressbook')
+admin.site.index_title = _('Dashboards')
+
+
+class AddressInline(admin.StackedInline):
+    model = Address
+    extra = 0
+    radio_fields = {
+        'role': admin.HORIZONTAL
+    }
+
+
+class PhoneInline(admin.TabularInline):
+    model = Phone
+    extra = 0
+
+
+class EmailInline(admin.TabularInline):
+    model = Email
+    extra = 0
 
 
 @admin.register(Person)
 class PersonAdmin(admin.ModelAdmin):
-    pass
+    inlines = [AddressInline, PhoneInline, EmailInline]
+    radio_fields = {
+        'gender': admin.VERTICAL
+    }
